ac39a0ba902e35a6de889bd3e91e762f15f5207f
jakub5016 <me.jakub.gil@gmail.com>
2023-12-12 22:06:41 +0100

    Func to get passwords and passwords as global var

--- Lista_9/hasz.c
+++ Lista_9/hasz.c
@@ -4,6 +4,8 @@
 #define PASS_NUM 5 // Number of passwords to collect
 #define WORD_NUM 15
 
+char PASSWORDS[PASS_NUM][33];
+
 
 void bytes2md5(const char *data, int len, char *md5buf) {
 	EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
@@ -33,28 +35,32 @@ int check_pass(char * pass, char * correct_pass){
     return isTheSame;
 }
 
-int main(){
-
-    FILE* hasla = fopen("hasla.txt", "r");
-    FILE* dir = fopen("dir.txt", "r");
-
+void get_passwords(char * nameOfTheFile){ //
+    FILE* hasla = fopen(nameOfTheFile, "r");
     char buf[BUFSIZE];
-    char passwords[PASS_NUM][33];
 
-    // Space = 32 in asci
     for (int i = 0; i < PASS_NUM; i++){
         fgets(buf, BUFSIZE, hasla);
         
         // GET PASSWORD HASHED
         for (int j = 5; j<= 33 + 5; j++){  // 5 first letters are id then we have a password that is 32 letters
             if ((buf[j] != 9) && (buf[j] != 32)){    // Space = 32, TAB = 9
-                passwords[i][j-5] = buf[j];
+                PASSWORDS[i][j-5] = buf[j];
             }
         }
-        passwords[i][32] = '\0';
+        PASSWORDS[i][32] = '\0';
 
-        printf("NEW PASWORD: %s\n", passwords[i]);
+        printf("NEW PASWORD: %s\n", PASSWORDS[i]);
     }
+
+    fclose(hasla);
+}
+
+int main(){
+
+    FILE* dir = fopen("dir.txt", "r");
+    
+    get_passwords("hasla.txt");
 
     // Getting a word from dir and trying to break password
     char word_got[BUFSIZE];
@@ -70,7 +76,6 @@ int main(){
         j=0;
         for (; j<BUFSIZE; j++){ 
             if (word_got[j] == '\n'){break;}
-            // printf("%c\n",word_got[j]);
         }
 
         char word[j];
@@ -85,8 +90,8 @@ int main(){
         // printf("%s\n" ,word);
 
         for (int j = 0; j < PASS_NUM; j++){
-            if (check_pass(word, passwords[j])){
-                printf("HASLO nr. %d, w md5 jest to %s, zostalo zlamane jest to \"%s\" w md5 jest to ", j, passwords[j], word);
+            if (check_pass(word, PASSWORDS[j])){
+                printf("HASLO nr. %d, w md5 jest to %s, zostalo zlamane jest to \"%s\" w md5 jest to ", j, PASSWORDS[j], word);
                 char md5[33]; // 32 characters + null terminator
                 bytes2md5(word, strlen(word), md5);
                 printf("%s\n", md5);
