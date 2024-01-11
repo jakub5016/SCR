52e3a1e4a17fcecb853257eb8f67f591f9ecda65
jakub5016 <me.jakub.gil@gmail.com>
2024-01-11 14:31:02 +0100

    final

--- Lista_10/delayer.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-while true; do
-    read line
-    sleep 1
-    echo $line x $1 x $2 x $3 
-done
--- Lista_10/zad.c
+++ Lista_10/zad.c
@@ -1,68 +1,235 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
+#define AMMOUT 10
 
-pthread_mutex_t compute;
+int iloscRobotow = 0;
+int kwantCzasu = 0;
+int typAlgorytmu = 2; //FCFS
 
-struct proscess_info
+// FCFS Var
+int lastTaczka = 0;
+// RR Var
+int kwantCzasuCounter = 0;
+
+struct taczka
 {
-    int number;
-    int exec_time;
+    int id, weight, ammout, timeToUnload;
+    char rockType[100];
 };
 
-void * SJF(){
-
+void printfTaczka(struct taczka pomocnicza){
+    printf("    wheelbarrow arrived <%d %s %d %d [%d]>\n", pomocnicza.id, pomocnicza.rockType, pomocnicza.weight, pomocnicza.ammout, pomocnicza.weight * pomocnicza.ammout);
 }
 
-void * FCFS(void * arg){ // First come first served
-    struct proscess_info * data = (struct proscess_info *)arg;
+struct taczka tablicaTaczek[1000];
+int pozycjaNaTablicy = 0;
 
-
-    pthread_mutex_lock(&compute);
-    int pocess_number_got = data->number;
-    printf("Proces nr.%d zostal uruchomiony\n", pocess_number_got);
-    pthread_mutex_unlock(&compute);
-    return NULL;
+void pop(int number){
+    pozycjaNaTablicy = pozycjaNaTablicy -1;
+    for (int i = number; i < pozycjaNaTablicy; i++)
+    {
+        tablicaTaczek[i] = tablicaTaczek[i+1];
+    }
+    
 }
 
-int main(int argc, char *argv[]) {// Argumenty wywolania 
-    if (argc < 3){
-        printf("Bad arg val\n");
+
+void RR(){
+    struct taczka pomocnicza;
+    printf("        ");
+    for (int i = 0; i < iloscRobotow; i++){
+        if ((i < pozycjaNaTablicy) && (tablicaTaczek[i].timeToUnload != 0)){
+            printf("[%s        %d]",tablicaTaczek[i].rockType, tablicaTaczek[i].timeToUnload);
+            tablicaTaczek[i].timeToUnload = tablicaTaczek[i].timeToUnload -1;
+        }
+        else{
+            printf("[        ]");
+        }
+        
+    }
+    if ((kwantCzasu-1) == kwantCzasuCounter){
+        for (int i = 0; i < iloscRobotow; i++)
+        {
+            if (i < pozycjaNaTablicy){
+                if (tablicaTaczek[0].timeToUnload != 0){
+                    pomocnicza = tablicaTaczek[0];
+                    pop(0);
+                    tablicaTaczek[pozycjaNaTablicy] = pomocnicza;
+                    pozycjaNaTablicy++; 
+                }
+                else{
+                    pop(0); 
+                }
+            }
+        }
+
+    }
+    kwantCzasuCounter = (kwantCzasuCounter +1)%kwantCzasu;
+   
+    printf("\n\n");
+}
+
+void FCFS(){
+    printf("        ");
+    for (int i = 0; i < iloscRobotow; i++){
+        if ((lastTaczka + i) < pozycjaNaTablicy){
+            while ((tablicaTaczek[lastTaczka + i].timeToUnload == 0) && ((lastTaczka + i) < pozycjaNaTablicy)) {
+                lastTaczka++;
+            }
+
+
+            printf("[%s        %d]",tablicaTaczek[lastTaczka + i].rockType, tablicaTaczek[lastTaczka + i].timeToUnload);
+            tablicaTaczek[lastTaczka + i].timeToUnload = tablicaTaczek[lastTaczka + i].timeToUnload -1;
+
+        }
+        else{
+            printf("[        ]");
+        }
+        
+    }
+    printf("\n\n");
+}
+
+int main(int argc, char *argv[]){
+    if (argc < 4){
+        printf("Niewystarczajaca ilosc argumentow wywoalania\n");
         return -1;
     }
-    int ilosc_watkow = atoi(argv[2]);
-    pthread_t watki[ilosc_watkow];
-    int rodzaj_strategii = atoi(argv[1]);
 
-    printf("ILOSC WATKOW: %d\nRODZAJ STRATEGII : %d\n", ilosc_watkow, rodzaj_strategii);
+    iloscRobotow = atoi(argv[1]);
+    printf("%d robots in the mine\n", iloscRobotow);
+    kwantCzasu = atoi(argv[2]);
+    FILE *fptr;
+    if (strcmp(argv[3], "-")){
+        fptr = fopen(argv[3],"read");
+        if (fptr == NULL){
+            printf("Nie istnieje plik o takiej nazwie\n");
+            return -1;
+        }
+    }
+    else{
+        fptr = stdin;
+    }
+    typAlgorytmu = atoi(argv[4]);
 
-    pthread_mutex_init(&compute, NULL);
+    char buffer[1000];
+    struct taczka pomocnicza;
 
-    struct proscess_info info;
+    int minute = 0;
+    int type = 0;
 
-    for(int i =0; i< ilosc_watkow; i++){
-        info.number = i;
 
-        // printf("rodzaj_strategii: %d\n", rodzaj_strategii);
-        switch (rodzaj_strategii)
+    while (fgets(buffer,sizeof(buffer), fptr) != NULL)
+    {
+        minute = buffer[0] - '0';
+        printf("Moment %d\n", minute);
+        type = 0;
+        for (int i = 2; ; i++){
+            if ((buffer[i] == '\0') || (buffer[i] == '\n')){
+                break;
+            }
+
+
+            if (buffer[i] != ' '){
+                switch (type)
+                {
+                case 0: // Szukamy ID
+                    pomocnicza.id = buffer[i] - '0';
+                    if ((((buffer[i + 1]) != ' ') && (buffer[i+1] != '\n')) && (buffer[i+1] != '\0')){
+                        pomocnicza.id = (pomocnicza.id*10) + (buffer[i+1] - '0');
+                        i++;
+                    }
+
+                    break;
+
+                case 1: // Szukamy slowa
+                    int j = 0;
+                    while (buffer[i] != ' ')
+                    {
+                        pomocnicza.rockType[j] = buffer[i];
+                        i++;
+                        j++;
+                    }
+                    pomocnicza.rockType[j] = '\0';
+                    i--;
+                    break;
+
+                case 2: // Szukamy wagi
+                    pomocnicza.weight = buffer[i] - '0';
+                    if ((((buffer[i + 1]) != ' ') && (buffer[i+1] != '\n')) && (buffer[i+1] != '\0')){
+                        pomocnicza.weight = (pomocnicza.weight*10) + (buffer[i+1] - '0');
+                        i++;
+                    }
+
+
+                    break;
+                
+                case 3: // Szukamy ilości i KOŃCZYMY PRZESZUKIWANIE TEGO KAMULCA
+                    pomocnicza.ammout = buffer[i] - '0';
+                    if ((((buffer[i + 1]) != ' ') && (buffer[i+1] != '\n')) && (buffer[i+1] != '\0')){
+                        pomocnicza.ammout = (pomocnicza.ammout*10) + (buffer[i+1] - '0');
+                        i++;
+                    }
+                    /// WYWAL TĄ TACZKEEEEEEEEEEE
+
+                    pomocnicza.timeToUnload = pomocnicza.ammout * pomocnicza.weight;
+                    tablicaTaczek[pozycjaNaTablicy] = pomocnicza;
+                    pozycjaNaTablicy++;
+                    printfTaczka(pomocnicza);
+                    break;
+                default:
+                    break;
+                }
+            }
+            else{
+                type = (type +1)%4;
+            }
+        }
+        switch (typAlgorytmu)
         {
-        case 0:
-            pthread_create(&watki[i], NULL, FCFS, (void *) & info);
+        case 2:
+            FCFS();
             break;
-        
         case 1:
-            info.exec_time = i * i; //EXAMPLE
-            pthread_create(&watki[i], NULL, SJF, (void *) & info );
+            RR();
             break;
         default:
-            printf("OKRESLONA STRATEGIA NIE ISTNIEJE\n");
-            return -1;
             break;
         }
     }
 
-    for(int i =0; i< ilosc_watkow; i++){
-        pthread_join(watki[i], NULL);
-    }    
+    int isEmpty = 0;
+    if (typAlgorytmu < 1 || typAlgorytmu > 2){
+        return -1;
+    }
+    while ((isEmpty == 0) && (minute < 30)) // Drugi argument to ułatwienie dla debugowania.
+    {
+        minute++;
+        isEmpty = 1;
+        for (int i = 0; i < pozycjaNaTablicy; i++)
+        {
+            if (tablicaTaczek[i].timeToUnload != 0){
+                isEmpty = 0;
+            }
+        }
+
+        if (isEmpty == 0){
+            printf("Moment %d\n", minute);
+            switch (typAlgorytmu)
+            {
+            case 2:
+                FCFS();
+                break;
+            case 1:
+                RR();
+                break;
+            default:
+                break;
+            }
+        }
+        
+    }
+    
     return 0;
 }
